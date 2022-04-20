108fe46805ea8b7b2bc4dd10fdbc1f7ff8a5b5e2
Matt Fellenz <matt@felle.nz>
2022-04-19 18:49:03 -0700

    Fix Actix implementation

--- src/actix.rs
+++ src/actix.rs
@@ -1,6 +1,25 @@
 use super::{BindBindableExt, BindableAddr};
 
-impl BindBindableExt for actix_web::HttpServer<_, _, _, _> {
+use actix_http::{Request, Response};
+use actix_service::{IntoServiceFactory, Service, ServiceFactory};
+use actix_web::body::MessageBody;
+use actix_web::dev::AppConfig;
+use actix_web::{Error, HttpServer};
+
+impl<F, I, S, B> BindBindableExt for HttpServer<F, I, S, B>
+where
+	F: Fn() -> I + Send + Clone + 'static,
+	I: IntoServiceFactory<S, Request>,
+
+	S: ServiceFactory<Request, Config = AppConfig> + 'static,
+	S::Error: Into<Error> + 'static,
+	S::InitError: std::fmt::Debug,
+	S::Response: Into<Response<B>> + 'static,
+	<S::Service as Service<Request>>::Future: 'static,
+	S::Service: 'static,
+
+	B: MessageBody + 'static,
+{
 	type Return = std::io::Result<Self>;
 
 	fn bind_bindable(self, addr: &BindableAddr) -> Self::Return {
